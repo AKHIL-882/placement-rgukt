328a159b12491cd07677c870579892282cc2a0a8
AKHIL-882 <51040667+AKHIL-882@users.noreply.github.com>
2022-08-06 21:27:05 +0530

    done

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>| Placement RGUKT IIIT SKLM |</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="vendor/aos/aos.css" rel="stylesheet">
+  <link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <section id="topbar" class="topbar d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">s160***@rguktsklm.ac.in</a></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+91 *******992</span></i>
+      </div>
+      <div class="social-links d-none d-md-flex align-items-center">
+        <a href="https://www.linkedin.com/in/duggirala-akhil-151768187/" target="blank" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+    </div>
+  </section><!-- End Top Bar -->
+
+  <header id="header" class="header d-flex align-items-center">
+
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+      <a href="index.html" class="logo d-flex align-items-center">
+        <h1>RGUKT - SKLM</h1>
+      </a>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="#hero">Home</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+
+    </div>
+  </header><!-- End Header -->
+  <!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero">
+    <div class="container position-relative">
+      <div class="row gy-5" data-aos="fade-in">
+        <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center text-center text-lg-start">
+          <h2>Welcome to <span>RGUKT IIIT SKLM  PLACEMENTS</span></h2>
+          <p> Placement Cell is a One-Stop Destination to skill - reskill yourself. More than 1000+ students trust and build strong careers</p>
+          <div class="d-flex justify-content-center justify-content-lg-start">
+            <a href="admin/login" class="btn-get-started">Admin Login</a> &nbsp;&nbsp;
+            <a href="login" class="btn-get-started p-3">Student Login</a>
+            
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2">
+          <img src="img/about-2.jpg" class="img-fluid" alt="" data-aos="zoom-out" data-aos-delay="100">
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Hero Section -->
+
+
+    <!-- ======= Stats Counter Section ======= -->
+    <section id="stats-counter" class="stats-counter">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <h2>Our Records</h2>
+          <p>One of the Unique features of Placement Cell is to conduct various test to ensure an in-depth understanding of the concepts & experience the pure joy of learning
+          </p>
+        </div>
+
+        <div class="row gy-4 align-items-center">
+
+          <div class="col-lg-6">
+            <img src="img/stats-img.svg" alt="" class="img-fluid">
+          </div>
+
+          <div class="col-lg-6">
+
+            <div class="stats-item d-flex align-items-center">
+              <span data-purecounter-start="0" data-purecounter-end="20" data-purecounter-duration="1" class="purecounter"></span>
+              <p><strong>Placement Partners</strong> </p>
+            </div><!-- End Stats Item -->
+
+            <div class="stats-item d-flex align-items-center">
+              <span data-purecounter-start="0" data-purecounter-end="800" data-purecounter-duration="1" class="purecounter"></span>
+              <p><strong>Placemented Candidates</strong></p>
+            </div><!-- End Stats Item -->
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Stats Counter Section -->
+
+    
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients">
+      <div class="container" data-aos="zoom-out">
+        <div class="clients-slider swiper">
+          <div class="swiper-wrapper align-items-center">
+            <div class="swiper-slide"><img src="https://www.topdevelopers.co/upload/202012071348451297118497.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/oo2jkovct71mibgzwfkh" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="https://economictimes.indiatimes.com/thumb/msid-58480138,width-1200,height-900,resizemode-4,imgsize-38943/wipro-launches-new-brand-identity.jpg?from=mdr" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="https://www.tcs.com/content/dam/tcs/images/Newtcslogo/tcs-logo-tata-blue-1x-dec2021.png" class="img-fluid" alt=""></div>
+            
+          </div>
+        </div>
+      </div>
+    </section><!-- End Clients Section -->
+
+    
+    
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="container">
+      <div class="row gy-4">
+        <div class="col-lg-5 col-md-12 footer-info">
+          <a href="index.html" class="logo d-flex align-items-center">
+            <span>RGUKT IIIT SKLM PLACEMENTS</span>
+          </a>
+          <p>Placement Cell is a One-Stop Destination to skill - reskill yourself. <br>More than 1000+ students trust and build strong careers</p>
+          <div class="social-links d-flex mt-4">
+
+          </div>
+        </div>
+
+        <div class="col-lg-2 col-6 footer-links">
+          <h4>Useful Links</h4>
+          <ul>
+            <li><a href="https://rguktsklm.ac.in/" target="blank">RGUKT IIIT Srikakulam</a></li>
+            <li><a href="#">RGUKT IIIT Nuzvid</a></li>
+            <li><a href="#">RGUKT IIIT RKV</a></li>
+            <li><a href="#">RGUKT IIIT Ongole</a></li>
+            <li><a href="#">RGUKT IIIT Basara</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-6 footer-links">
+          
+        </div>
+
+        <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+          <h4>Contact Us</h4>
+          <p>
+            Rajiv Gandhi University of Knowledge Technologies-Srikakulam Campus,<br>
+             S.M Puram, Etcherla(M)<br>
+              Srikakulam District,<br>
+              Andhra Pradesh - 532402
+          </p>
+
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container mt-4">
+      <hr>
+      <div class="copyright">
+        <strong><span>
+                <div class="credits">
+                    &copy; Designed and Developed by <a href="https://www.linkedin.com/in/duggirala-akhil-151768187/" target="_blank">| Akhil Duggirala |</a>
+                </div>
+            </span></strong> All Rights Reserved
+        <img src="https://hitwebcounter.com/counter/counter.php?page=8022360&style=0025&nbdigits=5&type=page&initCount=0" title="Free Counter" Alt="web counter" border="0" style="float:right; height:18px" />
+    </div>
+    </div>
+
+  </footer><!-- End Footer -->
+  <!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/aos/aos.js"></script>
+  <script src="vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
